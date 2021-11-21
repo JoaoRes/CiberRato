3d99da29be7d699ef37914cc03abe0ab81ff69cc
Joao Oliveira <joaooliveira@pop-os.localdomain>
2021-11-21 23:49:16 +0000

    k

--- mainRobC2.py
+++ mainRobC2.py
@@ -486,7 +486,7 @@ class MyRob(CRobLinkAngs):
         return walls
 
     def mapWriting(self):
-        file= open("mapping.out", 'w')    
+        file= open(ficheiro, 'w')    
         for i in range(1,28):
             for j in range(1,56):
                 if (j,i) in self.d:
@@ -531,6 +531,7 @@ rob_name = "pClient1"
 host = "localhost"
 pos = 1
 mapc = None
+ficheiro= ""
 
 for i in range(1, len(sys.argv),2):
     if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
@@ -542,7 +543,7 @@ for i in range(1, len(sys.argv),2):
     elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
         mapc = Map(sys.argv[i + 1])
     elif (sys.argv[i] == "--file" or sys.argv[i] == "-f") and i != len(sys.argv) - 1:
-        mapc = Map(sys.argv[i + 1])
+        ficheiro = (sys.argv[i + 1])
     else:
         print("Unkown argument", sys.argv[i])
         quit()
--- mainRobC3.py
+++ mainRobC3.py
@@ -571,6 +571,7 @@ rob_name = "pClient1"
 host = "localhost"
 pos = 1
 mapc = None
+ficheiro = ""
 
 for i in range(1, len(sys.argv),2):
     if (sys.argv[i] == "--host" or sys.argv[i] == "-h") and i != len(sys.argv) - 1:
@@ -582,7 +583,7 @@ for i in range(1, len(sys.argv),2):
     elif (sys.argv[i] == "--map" or sys.argv[i] == "-m") and i != len(sys.argv) - 1:
         mapc = Map(sys.argv[i + 1])
     elif (sys.argv[i] == "--file" or sys.argv[i] == "-f") and i != len(sys.argv) - 1:
-        mapc = Map(sys.argv[i + 1])
+        ficheiro = (sys.argv[i + 1])
     else:
         print("Unkown argument", sys.argv[i])
         quit()
